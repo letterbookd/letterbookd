3ef350d33dbaf57bf50c41f6fc47935cd89e2253
Wahyu Hidayat <wahyuhidayaat510@gmail.com>
2023-10-28 01:51:47 +0700

    fixed edit profile AJAX issue

--- reader/urls.py
+++ reader/urls.py
@@ -8,9 +8,15 @@ urlpatterns = [
     path('search/<str:name>', search_reader, name='search_reader'),
     path('edit-profile/', edit_profile, name='edit_profile'),
     path('settings/', user_settings, name='user_settings'),
-    path('apply-settings/', apply_settings, name='apply_settings'),
+    #path('apply-settings/', apply_settings, name='apply_settings'),
     path('search-catalog/', search_catalog, name='search_catalog'), 
     path('search-library/', search_library, name='search_library'), 
-    path('get-reader-data/<int:user_id>/', get_reader_data_by_user, name='get_reader_data_by_user'), 
-    path('update-reader-settings-ajax/', update_reader_settings_ajax, name='update_reader_settings_ajax'),
+    #path('get-reader-data/<int:user_id>/', get_reader_data_by_user, name='get_reader_data_by_user'), 
+    #path('update-reader-settings-ajax/', update_reader_settings_ajax, name='update_reader_settings_ajax'),
+    path('json/', show_json, name='show_json'),
+    path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
+    path('get-reader/', get_all_readers_json, name='get_all_readers_json'),
+    path('edit-profile-ajax/', edit_profile_ajax, name='edit_profile_ajax')
+    
+    
 ]
--- reader/views.py
+++ reader/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseNotFound, JsonResponse
+from django.http import HttpResponse, HttpResponseNotFound, JsonResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Reader
 from .forms import ReaderForm, ReaderPreferencesForm
@@ -6,6 +6,8 @@ from django.contrib.auth.models import User
 from library.models import *
 from catalog.models import *
 from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+from django.core import serializers
 
 # Menampilkan halaman profile Reader
 def show_profile(request, id):
@@ -43,21 +45,6 @@ def user_settings(request):
     
     return render(request, 'user_settings.html', context)
 
-# Mengapply setting untuk Reader
-def apply_settings(request):
-    if request.method == 'POST':
-        reader = get_object_or_404(Reader, user=request.user)
-        reader_form = ReaderForm(request.POST, instance=reader)
-        preferences_form = ReaderPreferencesForm(request.POST, instance=reader.preferences)
-        
-        if reader_form.is_valid() and preferences_form.is_valid():
-            reader_form.save()
-            preferences_form.save()
-            return JsonResponse({'status': 'success', 'message': 'Settings applied successfully'})
-        else:
-            return JsonResponse({'status': 'error', 'message': 'There was an error applying the settings'})
-    return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
-
 
 # Menampilkan hasil searching buku untuk suatu reader di catalog
 def search_catalog(request):
@@ -71,56 +58,31 @@ def search_library(request):
     user_books = LibraryBook.objects.filter(user=request.user, book__title__icontains=query)
     return render(request, 'book_search_results.html', {'results': user_books})
 
-# Mengembalikan data akun Reader untuk digunakan dalam bentuk JSON
-def get_reader_data_by_user(request, user_id):
-    reader = get_object_or_404(Reader, user__id=user_id)
-    
-    preferences = reader.preferences
-    
-    reader_data = {
-        'display_name': reader.display_name,
-        'bio': reader.bio,
-        'profile_picture': reader.profile_picture,
-        'personal_library': reader.personal_library,
-        'preferences': {
-            'share_reviews': preferences.share_reviews,
-            'share_library': preferences.share_library
-        }
-    }
-    
-    return JsonResponse(reader_data)
+def show_json(request):
+    all_readers = Reader.objects.all()
+    return HttpResponse(serializers.serialize("json", all_readers), content_type="application/json")
 
-from django.http import JsonResponse
+def show_json_by_id(request, id):
+    specific_reader = Reader.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", specific_reader), content_type="application/json")
 
-# Update data reader dengan AJAX 
+def get_all_readers_json(request):
+    all_readers = Reader.objects.all()
+    return HttpResponse(serializers.serialize('json', all_readers))
+
 @csrf_exempt
-def update_reader_settings_ajax(request):
+def edit_profile_ajax(request, id):
     if request.method == 'POST':
-        if not request.user.is_authenticated:
-            return JsonResponse({'status': 'error', 'message': 'User not authenticated'}, status=401)
-
         display_name = request.POST.get("display_name")
         bio = request.POST.get("bio")
-        profile_picture = request.POST.get("profile_picture")  
-        share_reviews = request.POST.get("share_reviews") == 'true'  
-        share_library = request.POST.get("share_library") == 'true' 
-        
-        try:
-            reader = Reader.objects.get(user=request.user)
-            
-            reader.display_name = display_name
-            reader.bio = bio
-            reader.profile_picture = profile_picture
-            reader.share_reviews = share_reviews
-            reader.share_library = share_library
 
-            reader.save()
+        reader = Reader.objects.get(pk = id)
 
-            return JsonResponse({'status': 'success', 'message': 'Settings updated successfully'})
+        reader.display_name = display_name
+        reader.bio = bio
+   
+        reader.save()
 
-        except Reader.DoesNotExist:
-            return JsonResponse({'status': 'error', 'message': 'Reader does not exist'}, status=404)
-        except Exception as e: 
-            return JsonResponse({'status': 'error', 'message': str(e)}, status=500)
+        return HttpResponse(b"CREATED", status=201)
 
-    return JsonResponse({'status': 'error', 'message': 'Invalid request method'}, status=400)
+    return HttpResponseNotFound()
