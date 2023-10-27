67d3c61d2bf85683b5d00b8eec942aa11719ea2c
mma <111668501+mmalkindi@users.noreply.github.com>
2023-10-27 19:15:49 +0700

    setting up library page and backend

--- library/models.py
+++ library/models.py
@@ -9,7 +9,7 @@ class Library(models.Model):
 class LibraryBook(models.Model):
     ''' Data untuk buku yang ada di Library '''
     
-    library = models.ForeignKey(Library, on_delete=models.CASCADE)
+    library = models.ForeignKey(Library, on_delete=models.CASCADE, related_name='mybooks')
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     tracking_status = models.IntegerField() # 0: UNTRACKED, 1: FINISHED, 2: READING, 3: ON HOLD, 4: PLANNED, 5: DROPPED
     is_favorited = models.BooleanField(default=False)
