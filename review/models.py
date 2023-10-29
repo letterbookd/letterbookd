0a93b462ca507718949530c60edcc148a2495d9b
Wahyu Hidayat <wahyuhidayaat510@gmail.com>
2023-10-29 08:46:24 +0700

    merging forum and review

--- review/models.py
+++ review/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 from library.models import *
 from reader.models import *
-from catalog.models import *
 
 # TODO
 class Review(models.Model):
     user = models.ForeignKey(Reader, on_delete=models.CASCADE)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, null=True)
     stars_rating = models.FloatField()
     status_on_review = models.CharField(max_length=255)
     date_posted = models.DateTimeField(auto_now_add=True)
