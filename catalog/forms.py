301640aad47d7ca42e303bc8e8d2eb355e288795
Wahyu Hidayat <wahyuhidayaat510@gmail.com>
2023-10-29 22:41:26 +0700

    retrieved files

--- catalog/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from catalog.models import *
-
-class BookForm(ModelForm):
-    class Meta:
-        model = Book
-        fields = ["isbn13", "title", "authors", "categories", "thumbnail", "description", "published_year", "page_count"]
